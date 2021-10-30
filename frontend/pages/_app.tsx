ac503646d80e929d14cfa11b6305f22be68607c5
Asto7 <ssatyanand7@gmail.com>
2021-10-30 12:30:43 +0530

    add: auth0

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,12 +2,19 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Auth0Provider
+      domain="dev-qhuj8gzm.us.auth0.com"
+      clientId="sD56Sagz3xKzHwU1wWHz0dKi2gvBw4t5"
+      redirectUri="http://localhost:3000"
+    >
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Auth0Provider>
   );
 }
 
